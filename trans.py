fa6d1f7fb5d340efde1aff01f7a22177fecb01f9
mletonsa <mika.letonsaari@gmail.com>
2024-08-25 22:45:21 +0700

    Updated for generalization

--- trans.py
+++ trans.py
@@ -12,13 +12,17 @@ with open("UTM.twee", "rt") as fin:
                     fout.write("Goto " + re.search('->(.*)]]', line).group(1)+";\n")
                 else:
                     fout.write ("GetSymbol"+";\n")
-                    fout.write ("Test symbol\n")
+                    fout.write ("Test q\n")
                     for i in range(len(x)):
                         fout.write("Case " + re.search('\[\[Read (.*)->', x[i]).group(1) + ":\n")
                         fout.write("    Goto " + re.search('->(.*)]]', x[i]).group(1)+";\n")
+                    fout.write("DEFAULT :\n")
+                    fout.write("    Goto Halt;\n")
+                    fout.write("ENDTEST\n")
             subline = re.search('You will write (.*) and', line)
             if subline:
-                fout.write("PutSymbol " + re.search('You will write (.*) and', line).group(1)+";\n")
-                fout.write("TurnRobot_" + re.search('and move (.*)\.', line).group(1)+";\n")
-                    
-                     
+                fout.write("SetSymbol (" + re.search('You will write (.*) and', line).group(1)+");\n")
+                if re.search('and move (.*)\.', line).group(1) == "left":
+                    fout.write("StepForward;\n")
+                else:
+                    fout.write("StepBack;\n")
